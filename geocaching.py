bc27a269555f71bce5e789df3007601a85d822ae
Tomas Bedrich <ja@tbedrich.cz>
2013-07-31 18:52:03 +0200

    improved login, added logout

--- geocaching.py
+++ geocaching.py
@@ -4,6 +4,7 @@ import logging
 import unittest
 import cookielib
 import urllib2
+import geopy as geo
 from urllib import urlencode
 from urlparse import urljoin
 from BeautifulSoup import BeautifulSoup
@@ -26,8 +27,8 @@ class Geocaching(object):
         # Installs a cookie jar and associates it with urllib2.
         # Important to keep us logged on to the website.
         # Adapted http://www.voidspace.org.uk/python/articles/cookielib.shtml
-        cj = cookielib.LWPCookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        self.cookieJar = cookielib.LWPCookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookieJar))
         urllib2.install_opener(opener)
 
 
@@ -38,52 +39,99 @@ class Geocaching(object):
 
         logging.info("Logging in...")
 
-        try:
+        page = self._urlopen(self._getURL("loginPage"))
+        if not page:
+            return False
+        soup = BeautifulSoup(page.read())
+
+        logging.debug("Checking for previous login.")
+        logged = self.getLoggedUser(soup)
+        if logged:
+            if logged == username:
+                logging.info("Already logged as %s.", logged)
+                self.loggedIn = True
+                return True
+            else:
+                logging.info("Already logged as %s, but want to log in as %s.", logged, username)
+                self.logout()
+        
+        # continue logging in
+        postValues = dict()
+        logging.debug("Assembling POST data.")
+
+        # login fields
+        loginElements = soup("input", type=["text", "password", "checkbox"])
+        for field, val in zip(loginElements, [username, password, 1]):
+            postValues[field["name"]] = val
+
+        # other nescessary fields
+        for field in soup("input", type=["hidden", "submit"]):
+            postValues[field["name"]] = field["value"]
+
+        url = self._getURL("loginPage")
+        headers = {"User-Agent": self.useragent}
+        postData = urlencode(postValues)
+
+        # login to the site
+        logging.debug("Submiting login form.")
+        page = self._urlopen(url, postData, headers)
+        if not page:
+            return False
+        soup = BeautifulSoup(page.read())
+
+        logging.debug("Checking the result.")
+        if self.getLoggedUser(soup):
+            logging.info("Logged in successfully as %s.", username)
+            self.loggedIn = True
+            return True
+        else:
+            logging.error("Cannot logon to the site (probably wrong username or password).")
+            return False
+
+    def logout(self):
+        """Logs out the user.
+
+        Logs out the user by cleaning cookies."""
+
+        logging.info("Logging out.")
+        self.cookieJar.clear()
+        
+
+    def getLoggedUser(self, soup=None):
+        """Returns the name of curently logged user.
+
+        Or None if no user is logged in."""
+
+        if not isinstance(soup, BeautifulSoup):
+            logging.debug("No 'soup' passed, loading login page on my own.")
             page = self._urlopen(self._getURL("loginPage"))
             soup = BeautifulSoup(page.read())
 
-            postValues = dict()
-            logging.debug("Assembling POST data.")
+        logging.debug("Checking for already logged user.")
+        try:
+            return soup.find("div", "LoggedIn").find("strong").text
+        except AttributeError:
+            return None
 
-            # login fields
-            loginElements = soup("input", type=["text", "password", "checkbox"])
-            for field, val in zip(loginElements, [username, password, 1]):
-                postValues[field["name"]] = val
 
-            # other nescessary fields
-            for field in soup("input", type=["hidden", "submit"]):
-                postValues[field["name"]] = field["value"]
+    def _urlopen(self, url, *args, **kwargs):
+        """Makes a urllib request.
 
-            url = self._getURL("loginPage")
-            headers = {"User-Agent": self.useragent}
-            postData = urlencode(postValues)
+        Returns opened stream of data or None on failure."""
 
-            # login to the site
-            logging.debug("Submiting login form.")
-            page = self._urlopen(url, postData, headers)
-            soup = BeautifulSoup(page.read())
-
-            logging.debug("Checking the result.")
-            if not soup("div", "LoggedIn"):
-                logging.error("Cannot logon to the site (probably wrong username or password).")
-                return False
-
-            logging.info("Logged in successfully as %s.", username)
-            return True
+        try:
+            logging.debug("Making request on: %s", url)
+            request = urllib2.Request(url, *args, **kwargs)
+            return urllib2.urlopen(request)
 
         except urllib2.URLError, e:
             logging.error("Cannot access the website: %s", e)
-            return False
+            return None
 
 
     def _getURL(self, name):
         return urljoin(self.baseurl, self.urls[name])
 
-
-    def _urlopen(self, url, *args, **kwargs):
-        logging.debug("Making request on: %s", url)
-        request = urllib2.Request(url, *args, **kwargs)
-        return urllib2.urlopen(request)
 
 
 
@@ -95,9 +143,25 @@ class TestGeocaching(unittest.TestCase):
         self.g = Geocaching()
 
     def test_login(self):
-        # we have to try bad username first (cookies)
+        # bad username
         self.assertFalse( self.g.login("", "") )
+        # good username
         self.assertTrue( self.g.login(self.username, self.password) )
+        # good username already logged
+        self.assertTrue( self.g.login(self.username, self.password) )
+        # bad username automatic logout
+        self.assertFalse( self.g.login("", "") )
+
+    def test_logout(self):
+        self.assertTrue( self.g.login(self.username, self.password) )
+        self.g.logout()
+        self.assertEquals( self.g.getLoggedUser(), None )
+
+    def test_getLoggedUser(self):
+        self.assertTrue( self.g.login(self.username, self.password) )
+        self.assertEquals( self.g.getLoggedUser(), self.username )
+
+
 
 
 def main():
