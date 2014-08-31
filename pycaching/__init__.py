6ce8819130de9d94d4413a423dd5e3fa04d7f0f8
Tomas Bedrich <ja@tbedrich.cz>
2014-08-31 20:51:33 +0200

    Changes in setup.py (removed dependency on the whole package)

--- pycaching/__init__.py
+++ pycaching/__init__.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
 
-__title__ = "pycaching"
-__version__ = "3.0.dev"  # PEP 386
-__author__ = "Tomas Bedrich"
-__license__ = "LGPL v3.0"
-__copyright__ = "Copyright 2014 Tomas Bedrich"
-
-
-__all__ = ["geocaching", "cache", "util", "point"]
-
+__all__ = ["geocaching", "cache", "util", "point", "errors"]
 
 from pycaching.geocaching import Geocaching  # NOQA
 from pycaching.cache import Cache  # NOQA
 from pycaching.util import Util  # NOQA
 from pycaching.point import Point  # NOQA
+from pycaching.errors import *  # NOQA
 
 
 def login(username, password):
--- setup.py
+++ setup.py
@@ -3,25 +3,22 @@
 import os
 from setuptools import setup
 
-from pycaching import __version__
-
-
 root = os.path.dirname(__file__) or "."
 f = open(os.path.join(root, "README.rst"))
 long_description = f.read()
 
 info = {
-    "name":                "PyCaching",
-    "version":             __version__,
+    "name":                "pycaching",
+    "version":             "3.0.dev",  # PEP 386
     "author":              "Tomas Bedrich",
     "author_email":        "ja@tbedrich.cz",
     "url":                 "https://github.com/tomasbedrich/pycaching",
     "packages":            ["pycaching"],
     "provides":            ["pycaching"],
-    "license":             "GNU Lesser General Public License (LGPL)",
-    "description":         "Geocaching.com site crawler. Searches and loads caches.",
+    "license":             "GNU Lesser General Public License (LGPL) v3.0",
+    "description":         "Geocaching.com site crawler. Provides tools for searching, fetching caches and geocoding.",
     "long_description":    long_description,
-    "keywords":            ["geocaching", "robot", "crawler", "geocache", "cache"],
+    "keywords":            ["geocaching", "crawler", "geocache", "cache", "searching", "geocoding"],
     "install_requires":    ["MechanicalSoup >= 0.2.0", "geopy >= 1.0.0"],
     "test_suite":          "test"
 }
