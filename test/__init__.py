0f434c7d3e8e8e56ffb8a06e76d26caa73097d06
jarhill0 <jarhill0@users.noreply.github.com>
2018-02-11 21:32:13 -0800

    Use pathlib properly

--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,5 @@
 import os
-import pathlib
+from pathlib import Path
 from urllib.parse import quote_plus
 
 from betamax import Betamax
@@ -11,10 +11,10 @@ from .helpers import sanitize_cookies
 username = os.environ.get('PYCACHING_TEST_USERNAME') or 'USERNAMEPLACEHOLDER'
 password = os.environ.get('PYCACHING_TEST_PASSWORD') or 'PASSWORDPLACEHOLDER'
 
-path = pathlib.Path('test', 'cassettes')
+path = str(Path('test/cassettes'))
 
 with Betamax.configure() as config:
-    config.cassette_library_dir = str(path)
+    config.cassette_library_dir = path
     config.define_cassette_placeholder('<USERNAME>', quote_plus(username))
     config.define_cassette_placeholder('<PASSWORD>', quote_plus(password))
     config.before_record(callback=sanitize_cookies)
