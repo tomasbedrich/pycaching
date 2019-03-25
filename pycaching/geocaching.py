d693fcf8431d6504ba6a8a3feff03c4ea691a969
Susanne Sauer <susanneTsauer@gmx.net>
2019-03-25 18:26:05 +0100

    get tests running (if not working)

--- pycaching/geocaching.py
+++ pycaching/geocaching.py
@@ -405,8 +405,8 @@ class Geocaching(object):
 
     @staticmethod
     def _get_date_from_string(string):
-        monthstrings = {"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6,
-                        "Jul":7, "Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12}
+        monthstrings = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6,
+                        "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
         day = int(string[:2])
         month = monthstrings[string[3:6]]
         year = int(string[-4:])
