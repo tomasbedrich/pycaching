44ef595811ae592b2af5ed3e30ac65596c6b61c0
BelKed <66956532+BelKed@users.noreply.github.com>
2026-03-08 06:53:19 +0100

    fix tests

--- test/__init__.py
+++ test/__init__.py
@@ -15,6 +15,7 @@ from .helpers import sanitize_cookies
 username = os.environ.get("PYCACHING_TEST_USERNAME") or "USERNAMEPLACEHOLDER"
 password = os.environ.get("PYCACHING_TEST_PASSWORD") or "PASSWORDPLACEHOLDER"
 cookie = os.environ.get("PYCACHING_TEST_COOKIE")
+auth_cookie_placeholder = "<AUTH COOKIE>"
 
 
 cassette_dir = Path("test/cassettes")
@@ -27,6 +28,30 @@ config.define_cassette_placeholder("<USERNAME>", quote_plus(username))
 config.define_cassette_placeholder("<PASSWORD>", quote_plus(password))
 config.before_record(callback=sanitize_cookies)
 Betamax.register_serializer(PrettyJSONSerializer)
+
+
+def _cassette_will_record(cassette_name, *, record=None):
+    if record in ("all", "new_episodes"):
+        return True
+
+    return not (cassette_dir / "{}.json".format(cassette_name)).exists()
+
+
+def _refresh_recording_auth(geocaching):
+    if not cookie and (username == "USERNAMEPLACEHOLDER" or password == "PASSWORDPLACEHOLDER"):
+        return
+
+    current_cookie = geocaching._session.cookies.get("gspkauth")
+    if current_cookie and current_cookie != auth_cookie_placeholder:
+        return
+
+    if cookie:
+        expected_username = None if username == "USERNAMEPLACEHOLDER" else username
+        geocaching.login_with_cookie(cookie, username=expected_username)
+    else:
+        geocaching._logged_in = False
+        geocaching._logged_username = None
+        geocaching.login(username, password)
 
 
 class NetworkedTest(unittest.TestCase):
@@ -60,3 +85,14 @@ class LoggedInTest(NetworkedTest):
             cls.gc._logged_in = True  # we're gonna trick it
             cls.gc._logged_username = username  # we're gonna trick it
             cls.gc._session = cls.session  # it got redefined; fix it
+
+        original_use_cassette = cls.recorder.use_cassette
+
+        def use_cassette_with_fresh_auth(cassette_name, *args, **kwargs):
+            # Setup cassettes can replace the live auth cookie with the scrubbed
+            # placeholder value. Refresh auth before recording the next cassette.
+            if _cassette_will_record(cassette_name, record=kwargs.get("record")):
+                _refresh_recording_auth(cls.gc)
+            return original_use_cassette(cassette_name, *args, **kwargs)
+
+        cls.recorder.use_cassette = use_cassette_with_fresh_auth
