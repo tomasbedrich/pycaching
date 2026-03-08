42bd756c7e3bb1565baa4f3b80c911ef74888e6f
BelKed <66956532+BelKed@users.noreply.github.com>
2026-03-08 11:47:11 +0100

    Improve Betamax cassette sanitization (#246)

--- tests_new/conftest.py
+++ tests_new/conftest.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
+from test.cassette_sanitizer import sanitize_betamax_interaction
 from urllib.parse import quote_plus
 
 import pytest
 import requests
 from betamax import Betamax
-from betamax.cassette.cassette import Placeholder
 from betamax_serializers.pretty_json import PrettyJSONSerializer
 
 from pycaching.geocaching import Geocaching
@@ -13,11 +13,6 @@ from pycaching.geocaching import Geocaching
 USERNAME = os.environ.get("PYCACHING_TEST_USERNAME") or "USERNAMEPLACEHOLDER"
 PASSWORD = os.environ.get("PYCACHING_TEST_PASSWORD") or "PASSWORDPLACEHOLDER"
 COOKIE = os.environ.get("PYCACHING_TEST_COOKIE")
-CLASSIFIED_COOKIES = (
-    "gspkauth",
-    "__RequestVerificationToken",
-    "jwt",  # NOTE: JWT token, contains user related informations: username, ids, oauth token
-)
 CASSETTE_DIR = Path(__file__).parent / "cassettes"
 
 
@@ -29,7 +24,7 @@ def betamax_config():
     _betamax_config.cassette_library_dir = str(CASSETTE_DIR)
     _betamax_config.define_cassette_placeholder("<USERNAME>", quote_plus(USERNAME))
     _betamax_config.define_cassette_placeholder("<PASSWORD>", quote_plus(PASSWORD))
-    _betamax_config.before_record(callback=_sanitize_betamax_cookies)
+    _betamax_config.before_record(callback=sanitize_betamax_interaction)
     _betamax_config.default_cassette_options["serialize_with"] = "prettyjson"
 
 
@@ -58,27 +53,3 @@ def geocaching_logged_in(betamax_session: requests.Session):
     else:
         gc.login(USERNAME, PASSWORD)
     return gc
-
-
-def _sanitize_betamax_cookies(interaction, cassette):
-    # TODO handle also request body occurence of __RequestVerificationToken
-    response = interaction.as_response()
-    response_cookies = response.cookies
-    request_cookies = dict()
-    for cookie in (interaction.as_response().request.headers.get("Cookie") or "").split("; "):
-        name, sep, val = cookie.partition("=")
-        if sep:
-            request_cookies[name] = val
-
-    secret_values = set()
-    for name in CLASSIFIED_COOKIES:
-        potential_val = response_cookies.get(name)
-        if potential_val:
-            secret_values.add(potential_val)
-
-        potential_val = request_cookies.get(name)
-        if potential_val:
-            secret_values.add(potential_val)
-
-    for val in secret_values:
-        cassette.placeholders.append(Placeholder(placeholder="<AUTH COOKIE>", replace=val))
