813f1cef02b5542c9f4eda5209bb35c38fe7ac92
BelKed <66956532+BelKed@users.noreply.github.com>
2026-03-11 19:13:58 +0100

    Fix regex pattern for extracting username (#248)

--- pycaching/geocaching.py
+++ pycaching/geocaching.py
@@ -273,7 +273,7 @@ class Geocaching(object):
 
         logging.debug("Checking for already logged user.")
         js_content = "\n".join(login_page.find_all(string=lambda i: isinstance(i, Script)))
-        m = re.search(r'"username":\s*"(.*)"', js_content)
+        m = re.search(r'"username"\s*:\s*"([^"]+)"', js_content)
         return m.group(1) if m else None
 
     def search(
